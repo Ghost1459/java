d6ccb7d4aa4b74d6b9f305b0d9b216a47bde0eb8
kr458133 <kd.rabiej@student.uw.edu.pl>
2026-03-30 13:39:56 +0200

    Initial commit

---
+++ untitled/src/Ludzie/Agent.java
@@ -0,0 +1,4 @@
+package Ludzie;
+
+public class Agent {
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+import Ludzie.Osoba;
+import Ludzie.Agent;
+import Ludzie.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Osoba[] table = new Osoba[0];
+        table[0] = new Osoba("James", "Bond");
+        table[1] = new Osoba("Jan","Kmicic");
+    }
+}
